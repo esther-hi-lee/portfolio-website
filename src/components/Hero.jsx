5c9882997cbaedccbc4341903a97a42465045017
Ryan <ryan@pal-nat186-107-18.itap.purdue.edu>
2026-01-15 14:22:04 -0500

    Changes splash text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ export default function Hero() {
     <section className="hero">
       <div className="container">
         <h1>Art Portfolio</h1>
-        <p>Splash text if you want it</p>
+        <p>A collection of my artwork showcasing various projects and styles.</p>
         <div style={{marginTop: '1rem'}}>
           <Link to="/gallery" className="btn">View Gallery</Link>
         </div>
